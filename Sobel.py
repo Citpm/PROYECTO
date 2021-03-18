42f20507092c710c4bf08f0919c2e76b33382cf5
Melissarobgar <80846124+Melissarobgar@users.noreply.github.com>
2021-03-18 11:53:54 -0700

    Update Sobel.py

--- Sobel.py
+++ Sobel.py
@@ -1,21 +1,26 @@
 "Modified by Melissa Robles García A01637961"
-import numpy as np
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
+
 def sobel(image,kernel):
-    image =cv2-cvtColor(image,cv2.COLOR_BGR2RGB)
-    fig, ax =plt.subplots(1,figsize=(10,8))
+    
+    image =cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    fig, ax =plt.subplots(1, figsize=(10,8))
     plt.imshow(image)
-    img =cv2.filter2D(image, -1, kernel)
+    img = cv2.filter2D(image, -1, kernel)
     fig, ax = plt.subplots(1, figsize=(10,8))
     plt.imshow(img)
 
+    
+    
+    
 if __name__ == "__main__":
-    image = cv2-imread("imagen.jpg")
-    cv2.imshow("imagen",image)
+    image = cv2.imread("imagen.jpg")
+    cv2.imshow("image",image)
     kernel = np.array ([[-1,-2,-1]
                         [0,0,0]
                         [1,2,1]])
-    output = sobel(image,kernel)
+    output = sobel(image, kernel)
     plt.show()
