e4666360050c56b612ef413e0013b6af62ea057b
diegorosasr <80845586+diegorosasr@users.noreply.github.com>
2021-03-18 11:48:10 -0600

    Add files via upload

---
+++ Sobel.py
@@ -0,0 +1,21 @@
+"Modified by Melissa Robles García A01637961"
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+def sobel(image,kernel):
+    image=cv2-cvtColor(image,cv2.COLOR_BGR2RGB)
+    fig, ax =plt.subplots(1,figsize=(10,8))
+    plt.imshow(image)
+    img=cv2.filter2D(image, -1, kernel)
+    fig, ax = plt.subplots(1, figsize=(10,8))
+    plt.imshow(img)
+
+if __name__ == "__main__":
+    image= cv2-imread("imagen.jpg")
+    cv2.imshow("imagen",image)
+    kernel = np.array ([[-1,-2,-1]
+                        [0,0,0]
+                        [1,2,1]])
+    output= sobel(image,kernel)
+    plt.show()
